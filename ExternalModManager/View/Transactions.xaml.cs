c18ed214e25279fa91868180ffdafd0df728cf72
KYPremco <KYPremco@gmail.com>
2023-09-09 13:09:57 +0200

    Test

--- ExternalModManager/View/Transactions.xaml.cs
+++ ExternalModManager/View/Transactions.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExternalModManager
+namespace ExternalModManager.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Transactions.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Transactions : UserControl
     {
-        public MainWindow()
+        public Transactions()
         {
             InitializeComponent();
-        }
-        
-        private void CloseApp_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
         }
     }
 }
